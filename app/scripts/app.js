25dc5a82e87137d788d2d748a03e495ef498daef
santtone <sami.anttonen@fifthelement.fi>
2015-03-24 23:37:31 +0200

    New dependency: ng-polymer-elements

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -2,5 +2,18 @@
 
 angular.module('folf', [
   'ui.router',
-  'ui.router.stateHelper'
+  'ui.router.stateHelper',
+  'ng-polymer-elements'
 ]);
+
+angular.module('ng-polymer-elements').constant('$ngPolymerMappings', {
+/*TODO
+newElement: {
+    anAttribute: {
+      primitive: 'an-attribute'
+    },
+    anEvent: {
+      event: 'an-event'
+    }
+  }*/
+});
