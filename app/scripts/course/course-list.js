961f6dd03e9c21aa7c295eb5b8b23d79ea1cfbfc
santtone <sami.anttonen@fifthelement.fi>
2015-03-28 23:19:17 +0200

    Adding courses to the list

--- app/scripts/course/course-list.js
+++ app/scripts/course/course-list.js
@@ -3,6 +3,18 @@
 angular.module('folf')
   .controller('CourseListCtrl', ['$scope', '$state', function ($scope, $state) {
 
+    $scope.courses = [
+      {
+        name: 'Urheilupuisto',
+        location: 'Kouvola'
+      },
+      {
+        name: 'Koivusaari',
+        location: 'Myllykoski, Kouvola'
+      }
+    ];
 
+    $scope.add = function(){
+    };
 
   }]);
